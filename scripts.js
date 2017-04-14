a05de6c7ad3906bc19b8bf12a346f23735f25f96
otis.scott <otismscott@gmail.com>
2017-04-14 10:31:15 -0700

    First commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+/**
+ * Created by h205p2 on 4/13/17.
+ */
+var vid = document.getElementById("bgvid");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed
+    vid.pause();
+// to capture IE10
+    vidFade();
+});
+
