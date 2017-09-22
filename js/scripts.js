e387c95d6ed9917a9b56e882afd1d3ea5384fb32
otis.scott <otismscott@gmail.com>
2017-09-22 09:47:47 -0700

    Layout functional for web browser, mobile and small display inbound. Added MDB files

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+/**
+ * Created by h205p2 on 4/13/17.
+ */
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+    if(window.location.pathname == '/ShareTube/index.html') {
+        location.href = "http://localhost:63342/ShareTube/postsignin.html"
+    }
+}
